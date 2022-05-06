5560c5ee3753ea4b8d45aa188daaf4d668cf617c
Brian Henning <henninb@msn.com>
2022-05-06 05:31:01 -0500

    wip

--- pages/temperature/index.tsx
+++ pages/temperature/index.tsx
@@ -1,79 +1,109 @@
 import Head from 'next/head'
-import { useState, useEffect } from 'react'
+import {useEffect, useState} from 'react'
 
 export default function Temperature() {
-        async function toFahrenheit(event) {
-            event.preventDefault()
-            let celsius = document.getElementById("celsius").innerText
-            console.log(celsius);
-            console.log(`celsius=${celsius}`);
+    const [fahrenheitState, setFahrenheitState] = useState({
+        fahrenheit: 0,
+        celsius: 0
+    });
 
-            const apiResponse = await fetch('/api/fahrenheit', {
-                method: 'POST',
-                body: JSON.stringify({celsius: celsius}),
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            });
+    const [celsiusState, setCelsiusState] = useState({
+        fahrenheit: 0,
+        celsius: 0
+    });
 
-            const result = await apiResponse.text();
-            console.log(result);
+
+    function handleFahrenheitChange(event) {
+        if (event.target.files) {
+            setFahrenheitState({...fahrenheitState, [event.target.name]: event.target.files[0]});
+        } else {
+            setFahrenheitState({...fahrenheitState, [event.target.name]: event.target.value});
         }
+    }
 
-        async function toCelsius(event) {
-            event.preventDefault()
-
-            let fahrenheit = document.getElementById("fahrenheit").innerText
-            console.log(`fahrenheit=${fahrenheit}`);
-            //let data = {fahrenheit: fahrenheit};
-            // const formData = new FormData(event.target);
-            // console.log("f=" + formData.get('fahrenheit'));
-            // Now you can use formData.get('foo'), for example.
-
-            const apiResponse = await fetch('/api/celsius', {
-                method: 'POST',
-                body: JSON.stringify({fahrenheit: fahrenheit}),
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            });
-
-            const result = await apiResponse.text();
-            console.log(result);
+    function handleCelsiusChange(event) {
+        if (event.target.files) {
+            setCelsiusState({...celsiusState, [event.target.name]: event.target.files[0]});
+        } else {
+            setCelsiusState({...celsiusState, [event.target.name]: event.target.value});
         }
+    }
 
-  useEffect(() => {
-    // if( !data) {
-    //   loadSchedule();
-    // }
-  }, [])
+    async function toFahrenheit(event) {
+        event.preventDefault()
+        //let celsius = document.getElementById("celsius").innerText
+        //console.log(celsius);
+        console.log(`fahrenheit=${JSON.stringify(celsiusState)}`);
 
-  return (
-    <div>
-      <Head>
-        <title>Temperature</title>
-        <meta name="description" content="" />
-      </Head>
+        const apiResponse = await fetch('/api/fahrenheit', {
+            method: 'POST',
+            body: JSON.stringify(celsiusState),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
 
-      <main>
-        <h1>Temperature</h1>
+        const result = await apiResponse.text();
+        console.log(result);
+    }
 
-          <div>
-              <form name="temperature-input">
-                  <label>fahrenheit</label>
-                  <input type="text" name="fahrenheit" id="fahrenheit" />
-                  <button onClick={toCelsius}>toCelsius</button>
-              </form>
+    async function toCelsius(event) {
+        event.preventDefault()
+        //let formData = new FormData();
 
-              <form name="temperature-input">
-                  <label>celsius</label>
-                  <input type="text" name="celsius" id="celsius" />
-                  <button onClick={toFahrenheit}>toFahrenheit</button>
-              </form>
-          </div>
+        console.log(`fahrenheit=${JSON.stringify(fahrenheitState)}`);
 
-      </main>
+        //let fahrenheit = document.getElementById("fahrenheit").innerText
+        //console.log(`fahrenheit=${fahrenheit}`);
+        //let data = {fahrenheit: fahrenheit};
+        // const formData = new FormData(event.target);
+        // console.log("f=" + formData.get('fahrenheit'));
+        // Now you can use formData.get('foo'), for example.
 
-    </div>
-  )
+        const apiResponse = await fetch('/api/celsius', {
+            method: 'POST',
+            body: JSON.stringify(fahrenheitState),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+
+        const result = await apiResponse.text();
+        console.log(result);
+    }
+
+    useEffect(() => {
+        // if( !data) {
+        //   loadSchedule();
+        // }
+    }, [])
+
+    return (
+        <div>
+            <Head>
+                <title>Temperature</title>
+                <meta name="description" content=""/>
+            </Head>
+
+            <main>
+                <h1>Temperature</h1>
+
+                <div>
+                    <form name="temperature-input">
+                        <label>fahrenheit</label>
+                        <input type="text" name="fahrenheit" id="fahrenheit" onChange={handleFahrenheitChange}/>
+                        <button onClick={toCelsius}>toCelsius</button>
+                    </form>
+
+                    <form name="temperature-input">
+                        <label>celsius</label>
+                        <input type="text" name="celsius" id="celsius" onChange={handleCelsiusChange}/>
+                        <button onClick={toFahrenheit}>toFahrenheit</button>
+                    </form>
+                </div>
+
+            </main>
+
+        </div>
+    )
 }
