94e7b38a26946a5ba7f3d17460dbeb3b913cbe46
Brian Henning <henninb@msn.com>
2022-05-09 05:46:12 -0500

    wip

--- pages/howto/index.tsx
+++ pages/howto/index.tsx
@@ -1,10 +1,10 @@
-export default function Docker() {
+export default function Howto() {
 
     return (
         <div>
             <h1>Docker</h1>
 
-            ## list docker logs
+            list docker logs
             <pre>
   <code>
             docker logs container
