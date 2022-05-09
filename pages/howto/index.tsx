5af8bd59afb1fc20d755aabd394172d7f9177d08
Brian Henning <henninb@msn.com>
2022-05-09 05:31:34 -0500

    wip

--- pages/howto/index.tsx
+++ pages/howto/index.tsx
@@ -1,3 +1,16 @@
-export default function Howto() {
+export default function Docker() {
 
+    return (
+        <div>
+            <h1>Docker</h1>
+
+            ## list docker logs
+            <pre>
+  <code>
+            docker logs container
+  </code>
+            </pre>
+
+        </div>
+    )
 }
