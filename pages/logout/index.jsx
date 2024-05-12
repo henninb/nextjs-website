dd0fdc8a9d0c2d814888ff8cfe7471c5c0cad71d
Brian Henning <henninb@msn.com>
2024-05-12 18:26:12 -0500

    wip

--- pages/logout/index.jsx
+++ pages/logout/index.jsx
@@ -1,6 +1,6 @@
 export default function Logout() {
 
-    sessionStorage.removeItem('isAuthenticated');
+    //sessionStorage.removeItem('isAuthenticated');
     console.log('logout');
     return (
       <div>
