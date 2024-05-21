4e54164dc62f903010bc07d233eae1d8d6487ef1
Brian Henning <henninb@msn.com>
2024-05-21 07:27:05 -0500

    wip

--- pages/api/nba-old.js
+++ pages/api/nba-old.js
@@ -1,9 +1,11 @@
-import {NextResponse} from 'next/server';
-
 export const runtime = 'edge';
 
-export default async function GET() {
+export default async function HockeyScores(request, response) {
+    // const token = request.headers.get('authorization')?.split(" ")[1] || '';
+    // console.log(token);
+
     const url = new URL('https://fixturedownload.com/feed/json/nba-2023/minnesota-timberwolves')
+
     const params = {};
 
     url.search = new URLSearchParams(params).toString();
@@ -14,6 +16,8 @@ export default async function GET() {
             "Content-Type": "application/json",
         },
     });
-    const response = await apiResponse.json();
-    return NextResponse.json(response);
+    //console.log('apiCall was made.');
+    const json = await apiResponse.json();
+    // console.log(json);
+    response.status(200).json(json)
 }
