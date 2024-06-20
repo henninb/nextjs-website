94a3f973926cea69ff277eaf7c961eadbceddb44
Brian Henning <henninb@gmail.com>
2024-06-20 10:09:09 -0500

    wip

--- pages/_document.js
+++ pages/_document.js
@@ -26,6 +26,20 @@ class MyDocument extends Document {
             src="//client.px-cloud.net/PXjJ0cYtn9/main.min.js"
             async
           ></script>
+
+<script>
+    // PX Cookie Syncing
+    !function(){
+        // Configuration
+        // var customCookieHeader = "x-px-cookies";
+        // var cookiesToSync = ["_px2", "_px3", "_pxhd", "_pxvid", "pxcts"];
+        // var domainsToSync = []; // TODO: add domains that should be synced with PX cookies (e.g. ["example1.com", "api.example2.com"])
+        // Implementation
+        // var e=customCookieHeader,t=cookiesToSync,n=domainsToSync;if(e&&t.length&&n.length){var r=XMLHttpRequest.prototype.open;if(XMLHttpRequest.prototype.open=function(){r.apply(this,arguments);try{if(c(arguments[1])){var t=o();t&&this.setRequestHeader(e,t)}}catch(n){}},window.fetch){var i=window.fetch;window.fetch=function(){try{if(c(arguments[0])){var t=o();t&&(arguments[1]||1!==arguments.length||(arguments[1]={},arguments.length=2),arguments[1].headers||(arguments[1].headers={}),arguments[1].headers[e]=t)}}catch(n){}return i.apply(this,arguments)}}}function c(e){var t=document.createElement("a");return t.href=e,n.some(function(e){return t.hostname.indexOf(e)>-1})}function o(){return document.cookie.split(/;\s?/).reduce(function(e,n){var r=n.split("=")[0];return t.indexOf(r)>-1&&(e+=n+"; "),e},"").slice(0,-2)}
+    }();
+</script>
+
+
           <script src="https://henninb.github.io/human-challenge/human-challenge.js"></script>
           <script
             src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
--- pages/lead/info/index.jsx
+++ pages/lead/info/index.jsx
@@ -46,6 +46,43 @@ export default function Info() {
     }
   };
 
+  const handleSubmitNew = async (e) => {
+    e.preventDefault();
+    const data = {
+      vin,
+      color,
+      name,
+      email,
+    };
+    console.log("Claim submitted:", data);
+    try {
+      process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+      const response = await fetch(
+        "https://f5x3msep1f.execute-api.us-east-1.amazonaws.com/prod/api-lead",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        }
+      );
+
+      const result = await response.json();
+
+      if (response.ok) {
+        console.log("Lead generated successfully:", result);
+        setResponseMessage("Lead generated successfully!");
+      } else {
+        console.error("Failed to generate lead:", result);
+        setResponseMessage("Failed to generate lead.");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      setResponseMessage("An error occurred while generating the lead.");
+    }
+  };
+
   return (
     <div>
       <h1>Enter Your Information</h1>
@@ -66,6 +103,25 @@ export default function Info() {
         />
         <button type="submit">Submit</button>
       </form>
+    <div>
+      <form onSubmit={handleSubmitNew}>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Enter Name"
+          required
+        />
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Enter Email"
+          required
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
       {responseMessage && <p>{responseMessage}</p>}
     </div>
   );
