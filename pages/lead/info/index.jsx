9987d5692db4a1016ac7fa82c06460c49e41849e
Brian Henning <henninb@gmail.com>
2024-06-21 12:30:06 -0500

    wip

--- pages/lead/info/index.jsx
+++ pages/lead/info/index.jsx
@@ -61,6 +61,7 @@ export default function Info() {
         {
           method: "POST",
           headers: {
+            "Accept": "application/json",
             "Content-Type": "application/json",
           },
           body: JSON.stringify(data),
