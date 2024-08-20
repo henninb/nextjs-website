7d5b7edcd8316ba61e052e92c2bea88e73d3359f
Brian Henning <henninb@gmail.com>
2024-08-20 08:39:53 -0500

    wip

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
 import "../styles/index.css";
 import Layout from "../components/Layout";
 import AuthProvider from "../components/AuthProvider";
+import { useEffect } from 'react';
 // import AuthProvider from "../components/AuthProvider";
 
 export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    window._pxCustomAbrDomains = ['amazonaws.com', 'execute-api.us-east-1.amazonaws.com'];
+  }, []);
+
   return (
     <div>
       <Layout>
