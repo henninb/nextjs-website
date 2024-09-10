7b04d388d44b7d564cf623ddf9e1507ed5c95ef8
Brian Henning <henninb@gmail.com>
2024-09-10 12:06:18 -0500

    wip

--- pages/lead/info/index.jsx
+++ pages/lead/info/index.jsx
@@ -58,6 +58,8 @@ export default function Info() {
         <script type="text/javascript" async="" src="https://prod.api.firstdata.com/ucom/v2/static/v2/js/ucom-sdk.js"></script>
         <script type="text/javascript" async="" src="https://snap.licdn.com/li.lms-analytics/insight.min.js"></script>
         <script type="text/javascript" async="" src="https://connect.facebook.net/en_US/fbevents.js"></script>
+        <script type="text/javascript" async="" src="https://track.sv.rkdms.com/js/sv.js"></script>
+        <script type="text/javascript" async="" src="https://www.clarity.ms/s/0.7.45/clarity"></script>
         <script type="text/javascript" async="" src="https://bat.bing.com/bat.js"></script>
         <script type="text/javascript" async="" src="https://js.hubspot.com/web-interactives-embed.js" id="hubspot-web-interactives-loader" crossorigin="anonymous" data-loader="hs-scriptloader" data-hsjs-portal="8187955" data-hsjs-env="prod" data-hsjs-hublet="na1"></script>
         <script
