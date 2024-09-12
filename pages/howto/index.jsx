0b0614dca5d75ac82cba8749f516dc40c3db55fa
Brian Henning <henninb@gmail.com>
2024-09-12 13:25:06 -0500

    wip

--- pages/howto/index.jsx
+++ pages/howto/index.jsx
@@ -8,15 +8,16 @@ export default function Howto() {
         <title>Howto - Brian Henning</title>
       </Head>
       <h1>Howto</h1>
-      <p>For detailed Docker instructions, please visit the Docker Howto page.</p>
-      <Link href="/howto/docker">Docker</Link>
-      <Link href="/howto/cloudflare">Cloudflare</Link>
-      <Link href="/howto/debian">Debian</Link>
-      <Link href="/howto/f5">F5</Link>
-      <Link href="/howto/gentoo">Gentoo</Link>
-      <Link href="/howto/nextjs">NextJS</Link>
-      <Link href="/howto/proxmox">Proxmox</Link>
-      <Link href="/howto/pfsense">pfSense</Link>
+      <ul>
+        <li><Link href="/howto/docker">Docker</Link></li>
+        <li><Link href="/howto/cloudflare">Cloudflare</Link></li>
+        <li><Link href="/howto/debian">Debian</Link></li>
+        <li><Link href="/howto/f5">F5</Link></li>
+        <li><Link href="/howto/gentoo">Gentoo</Link></li>
+        <li><Link href="/howto/nextjs">NextJS</Link></li>
+        <li><Link href="/howto/proxmox">Proxmox</Link></li>
+        <li><Link href="/howto/pfsense">pfSense</Link></li>
+      </ul>
     </div>
   );
 }
