1b1cc5fe1e67e02479f725b3f7274ed3426f6a3e
Brian <henninb@gmail.com>
2024-12-10 08:23:50 -0600

    fix-first-party

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <script src="//jJ0cYtn9/init.js" async></script>
+          <script src="/jJ0cYtn9/init.js" async></script>
 
           <script src="https://henninb.github.io/human-challenge/human-challenge.js"></script>
           <script type="applicatoin/javascript" src="https://henninb.github.io/human-challenge/hello.js"></script>
