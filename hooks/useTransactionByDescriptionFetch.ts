0cb831c02c1544b6714fdfd1b0905cab6dcb2ecf
Brian Henning <henninb@msn.com>
2025-02-15 11:55:36 -0600

    wip

---
+++ hooks/useTransactionByDescriptionFetch.ts
@@ -0,0 +1,135 @@
+import { useQuery } from "@tanstack/react-query";
+import Transaction from "../model/Transaction";
+//import { basicAuth } from "../Common";
+
+const dataTest: Transaction[] = [
+  {
+    transactionId: 10544,
+    guid: "299b36b1-a49f-43bc-aaa5-ba78352f716a",
+    accountId: 1029,
+    accountType: "credit",
+    transactionType: "undefined",
+    accountNameOwner: "barclay-cash_brian",
+    transactionDate: new Date("2017-09-17"),
+    description: "balance adjustment",
+    category: "test",
+    amount: 1.99,
+    transactionState: "outstanding",
+    activeStatus: true,
+    reoccurringType: "onetime",
+    notes: "",
+  },
+  {
+    transactionId: 10543,
+    guid: "55dd8ccb-b51c-4c93-907e-95ed1d20705f",
+    accountId: 1029,
+    accountType: "credit",
+    transactionType: "expense",
+    accountNameOwner: "barclay-cash_brian",
+    transactionDate: new Date("2017-08-18"),
+    description: "amazon.com",
+    category: "test",
+    amount: 0.99,
+    transactionState: "cleared",
+    activeStatus: true,
+    reoccurringType: "onetime",
+    notes: "egiftcard",
+  },
+  {
+    transactionId: 10542,
+    guid: "499cee31-ec4c-4f8a-b5b5-35ea9c9df9f4",
+    accountId: 1029,
+    accountType: "credit",
+    transactionType: "undefined",
+    accountNameOwner: "barclay-cash_brian",
+    transactionDate: new Date("2017-08-17"),
+    description: "balance adjustment",
+    category: "none",
+    amount: -0.99,
+    transactionState: "cleared",
+    activeStatus: true,
+    reoccurringType: "onetime",
+    notes: "test",
+  },
+  {
+    transactionId: 10541,
+    guid: "6128e4be-932d-4da3-af3b-3c25e76a9de9",
+    accountId: 1029,
+    accountType: "credit",
+    transactionType: "expense",
+    accountNameOwner: "barclay-cash_brian",
+    transactionDate: new Date("2017-07-18"),
+    description: "amazon.com",
+    category: "none",
+    amount: 0.99,
+    transactionState: "cleared",
+    activeStatus: true,
+    reoccurringType: "onetime",
+    notes: "",
+  },
+  {
+    transactionId: 10540,
+    guid: "798e8a7f-f615-46b1-aeba-b0b8f74e5d10",
+    accountId: 1029,
+    accountType: "credit",
+    transactionType: "undefined",
+    accountNameOwner: "barclay-cash_brian",
+    transactionDate: new Date("2017-07-17"),
+    description: "balance adjustment",
+    category: "none",
+    amount: -0.99,
+    transactionState: "cleared",
+    activeStatus: true,
+    reoccurringType: "onetime",
+    notes: "",
+  },
+];
+
+const fetchTransactionsByDescription = async (
+  description: string,
+): Promise<Transaction[] | null> => {
+  try {
+    const response = await fetch(
+      `https://finance.lan/api/transaction/description/${description}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          //Authorization: basicAuth(),
+        },
+      },
+    );
+
+    if (!response.ok) {
+      if (response.status === 404) {
+        console.log("Resource not found (404).")
+      }
+      throw new Error(
+        `Failed to fetch transactionsByDescription data: ${response.statusText}`,
+      );
+    }
+
+    return response.status !== 204 ? await response.json() : null;
+  } catch (error) {
+    console.log("Error fetching transactionsByDescription data:", error);
+    return dataTest; // Default fallback data on error
+  }
+};
+
+export default function useTransactionByDescriptionFetch(
+  accountNameOwner: string,
+) {
+  const queryResult = useQuery({
+    queryKey: ["descriptions", accountNameOwner],
+    queryFn: () => fetchTransactionsByDescription(accountNameOwner),
+  });
+  if (queryResult.isError) {
+    console.log(
+      "Error occurred while fetching transaction data:",
+      queryResult.error?.message,
+    );
+  }
+
+  return queryResult;
+}
---
+++ pages/finance/transactions/description/[descriptionName].tsx
@@ -0,0 +1,132 @@
+import { useRouter } from "next/router";
+import React, { useState, useEffect } from "react";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import Spinner from "../../../../components/Spinner";
+import SnackbarBaseline from "../../../../components/SnackbarBaseline";
+import useTransactionByDescription from "../../../../hooks/useTransactionByDescriptionFetch";
+import useTransactionUpdate from "../../../../hooks/useTransactionUpdate";
+import Transaction from "../../../../model/Transaction";
+import { Box } from "@mui/material";
+import Link from "next/link";
+
+export default function TransactionTable() {
+  const [showSpinner, setShowSpinner] = useState(true);
+  const [snackbarMessage, setMessage] = useState("");
+  const [showSnackbar, setShowSnackbar] = useState<boolean>(false);
+
+  const router = useRouter();
+  const { descriptionName }: any = router.query;
+
+  const {
+    data: fetchedTransactions,
+    isSuccess: isSucessTransactions,
+    error: errorTransactions,
+  } = useTransactionByDescription(descriptionName);
+  const { mutateAsync: updateTransaction } = useTransactionUpdate();
+
+  useEffect(() => {
+    if (isSucessTransactions) {
+      setShowSpinner(false);
+    }
+  }, [isSucessTransactions]);
+
+  const handleSnackbarClose = () => setShowSnackbar(false);
+
+  const handleError = (error: any, moduleName: string) => {
+    const errorMessage = error.response
+      ? `${moduleName}: ${error.response.status} - ${JSON.stringify(error.response.data)}`
+      : `${moduleName}: Failure`;
+    setMessage(errorMessage);
+    setShowSnackbar(true);
+  };
+
+  const columns: GridColDef[] = [
+    {
+      field: "transactionDate",
+      headerName: "Transaction Date",
+      type: "date",
+      width: 100,
+      renderCell: (params) => {
+        return params.value.toLocaleDateString("en-US");
+      },
+      valueGetter: (params: string) => {
+        const utcDate = new Date(params);
+        const localDate = new Date(
+          utcDate.getTime() + utcDate.getTimezoneOffset() * 60000,
+        );
+        return localDate;
+      },
+      editable: true,
+    },
+    {
+      field: "accountNameOwner",
+      headerName: "Account",
+      width: 180,
+      editable: true,
+      renderCell: (params) => {
+        return (
+          <Link href={`/finance/transactions/${params.row.accountNameOwner}`}>
+            {params.value}
+          </Link>
+        );
+      },
+    },
+    {
+      field: "description",
+      headerName: "Description",
+      width: 180,
+      editable: true,
+    },
+    { field: "category", headerName: "Category", width: 150, editable: true },
+    {
+      field: "amount",
+      headerName: "Amount",
+      type: "number",
+      width: 90,
+      editable: true,
+    },
+    { field: "activeStatus", headerName: "Status", width: 100, editable: true },
+    { field: "notes", headerName: "Notes", width: 200, editable: true },
+  ];
+
+  return (
+    <Box>
+      <h2>{`${descriptionName}`}</h2>
+      {showSpinner ? (
+        <Spinner />
+      ) : (
+        <div>
+          <DataGrid
+            rows={fetchedTransactions?.filter((row) => row != null) || []}
+            columns={columns}
+            getRowId={(row) => row.transactionId || 0}
+            checkboxSelection={false}
+            rowSelection={false}
+            processRowUpdate={async (
+              newRow: Transaction,
+              oldRow: Transaction,
+            ): Promise<Transaction> => {
+              if (JSON.stringify(newRow) === JSON.stringify(oldRow)) {
+                return oldRow;
+              }
+              try {
+                await updateTransaction({ newRow, oldRow });
+                setMessage("Transaction updated successfully.");
+                setShowSnackbar(true);
+                return { ...newRow };
+              } catch (error) {
+                handleError(error, "Update Transaction failure.");
+                throw error;
+              }
+            }}
+          />
+          <SnackbarBaseline
+            message={snackbarMessage}
+            state={showSnackbar}
+            handleSnackbarClose={handleSnackbarClose}
+          />
+        </div>
+      )}
+    </Box>
+  );
+}
