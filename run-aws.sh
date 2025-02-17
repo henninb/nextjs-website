e734fb25d8b857b22a5fd76f589c85394638ab0f
Brian Henning <henninb@msn.com>
2025-02-17 14:56:55 -0600

    updates

---
+++ run-aws.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+echo npx amplitude init
+echo npx amplify configure
+
+aws iam list-attached-user-policies --user-name henninb
+
+aws iam attach-user-policy \
+  --user-name henninb \
+  --policy-arn arn:aws:iam::aws:policy/AWSAmplifyAdminAccess
+
+aws amplify list-apps --query 'apps[*].appId'
+
+aws amplify create-domain-association --app-id d2ykjuyo12kys7 --domain-name bhenning.com \
+  --sub-domain-settings "[{\"prefix\":\"amplify\",\"branchName\":\"main\"}]"
+
+aws amplify get-domain-association --app-id d2ykjuyo12kys7 --domain-name bhenning.com
+
+exit 0
