f405cb09e0f1a1a54764250f8cecc880dbcd3eaa
Brian Henning <henninb@msn.com>
2025-03-26 06:43:06 -0500

    wip

--- pages/logout/index.tsx
+++ pages/logout/index.tsx
@@ -2,17 +2,17 @@ import { useEffect } from "react";
 import { useRouter } from "next/router";
 import cookie from "js-cookie";
 
-const Logout = () => {
+
+export default function Logout() {
+
   const router = useRouter();
 
   useEffect(() => {
-    // Remove the token cookie
-    cookie.remove("token");
-    // Redirect to the home page after clearing the token
+    // Remove the token cookie with the same domain and path as when it was set
+    cookie.remove("token", { domain: ".bhenning.com", path: "/" });
+    // Redirect to the login page after clearing the token
     router.push("/login");
   }, [router]);
 
   return null;
 };
-
-export default Logout;
