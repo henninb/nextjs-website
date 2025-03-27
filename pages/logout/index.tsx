57f41acd7c7ff0bfd12472e97ba3d5e92174fa57
Brian Henning <henninb@msn.com>
2025-03-26 19:22:49 -0500

    wip

--- pages/logout/index.tsx
+++ pages/logout/index.tsx
@@ -1,18 +1,12 @@
 import { useEffect } from "react";
-import { useRouter } from "next/router";
-import cookie from "js-cookie";
 import useLogout from "../../hooks/useLogoutProcess";
 
 export default function Logout() {
-  const router = useRouter();
-  //const { mutateAsync: u } = useTransferInsert();
+  const { logout } = useLogout();
 
   useEffect(() => {
-    // Remove the token cookie with the same domain and path as when it was set
-    cookie.remove("token", { domain: ".bhenning.com", path: "/" });
-    // Redirect to the login page after clearing the token
-    router.push("/login");
-  }, [router]);
+    logout();
+  }, [logout]);
 
   return null;
-};
+}
