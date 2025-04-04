9b7489d58a182f31049a80f696ecba45e3405ad3
Brian Henning <henninb@msn.com>
2025-04-03 19:34:05 -0500

    wip

---
+++ pages/me.tsx
@@ -0,0 +1,29 @@
+// pages/me.tsx
+import { NextPage } from "next";
+import { useUser } from "../hooks/useUser";
+
+const MePage: NextPage = () => {
+  const { user, isLoading, isError } = useUser();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error loading user data.</div>;
+
+  return (
+    <div>
+      <h1>My Profile</h1>
+      <div>
+        <p>
+          <strong>Username:</strong> {user.username}
+        </p>
+        {user.email && (
+          <p>
+            <strong>Email:</strong> {user.email}
+          </p>
+        )}
+        {/* Display additional user fields as needed */}
+      </div>
+    </div>
+  );
+};
+
+export default MePage;
