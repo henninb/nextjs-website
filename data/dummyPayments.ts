64530d9d9258edb7271a3e0ecb536cbf04e362e3
Brian Henning <henninb@msn.com>
2025-04-06 14:16:40 -0500

    wip

--- data/dummyPayments.ts
+++ data/dummyPayments.ts
@@ -4,6 +4,8 @@ export const dummyPayments: Payment[] = [
   {
     paymentId: 3001,
     accountNameOwner: "macys_brian",
+    sourceAccount: "bank_brian",
+    destinationAccount: "macys_brian",
     transactionDate: new Date("2024-10-01"),
     amount: 25.0,
     activeStatus: true,
@@ -11,6 +13,8 @@ export const dummyPayments: Payment[] = [
   {
     paymentId: 3002,
     accountNameOwner: "amex_brian",
+    sourceAccount: "bank_brian",
+    destinationAccount: "amex_brian",
     transactionDate: new Date("2024-10-02"),
     amount: 50.0,
     activeStatus: true,
@@ -18,6 +22,8 @@ export const dummyPayments: Payment[] = [
   {
     paymentId: 3003,
     accountNameOwner: "chase_brian",
+    sourceAccount: "bank_brian",
+    destinationAccount: "chase_brian",
     transactionDate: new Date("2024-10-03"),
     amount: 75.0,
     activeStatus: true,
@@ -25,6 +31,8 @@ export const dummyPayments: Payment[] = [
   {
     paymentId: 3004,
     accountNameOwner: "boa_brian",
+    sourceAccount: "bank_brian",
+    destinationAccount: "boa_brian",
     transactionDate: new Date("2024-10-04"),
     amount: 100.0,
     activeStatus: true,
@@ -32,6 +40,8 @@ export const dummyPayments: Payment[] = [
   {
     paymentId: 3005,
     accountNameOwner: "citibank_brian",
+    sourceAccount: "bank_brian",
+    destinationAccount: "citibank_brian",
     transactionDate: new Date("2024-10-05"),
     amount: 125.0,
     activeStatus: true,
