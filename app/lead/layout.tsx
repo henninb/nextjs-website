35530d9d1210ea33a147dfc1b2fb97e2db81585c
Brian <henninb@gmail.com>
2026-03-06 11:25:56 -0600

    updates

--- app/lead/layout.tsx
+++ app/lead/layout.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import type { Metadata } from "next";
 
+export const dynamic = "force-dynamic";
+
 export const metadata: Metadata = {
   title: {
     default: "Vehicle Lead Form",
