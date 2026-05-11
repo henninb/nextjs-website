8994895bc7cf61db0d672c2ddcc5d329162e9e41
Brian Henning <henninb@msn.com>
2026-05-10 19:04:58 -0500

    wip

--- utils/parseTransactionPaste.ts
+++ utils/parseTransactionPaste.ts
@@ -520,7 +520,7 @@ export function parseTransactionPaste(raw: string, accountType?: string): Parsed
   }
 
   const US_STATE_SUFFIX =
-    /\s+(AL|AK|AZ|AR|CA|CO|CT|DE|FL|GA|HI|ID|IL|IN|IA|KS|KY|LA|ME|MD|MA|MI|MN|MS|MO|MT|NE|NV|NH|NJ|NM|NY|NC|ND|OH|OK|OR|PA|RI|SC|SD|TN|TX|UT|VT|VA|WA|WV|WI|WY|DC)$/;
+    /\s+(AL|AK|AZ|AR|CA|CO|CT|DE|FL|GA|HI|ID|IL|IN|IA|KS|KY|LA|ME|MD|MA|MI|MN|MS|MO|MT|NE|NV|NH|NJ|NM|NY|NC|ND|OH|OK|OR|PA|RI|SC|SD|TN|TX|UT|VT|VA|WA|WV|WI|WY|DC|US)$/;
 
   // Trim "ONLINE TRANSFER" descriptions: keep those two words, move the rest to notes.
   // Strip trailing US state abbreviations from all descriptions.
